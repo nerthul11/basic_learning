79f76526b030c86e455cac8975e85a22dc46c36b
nerthul11 <leandrorositto@yahoo.com.ar>
2020-10-05 13:26:45 -0300

    Added CSV Maker file

---
+++ CSV Maker.py
@@ -0,0 +1,12 @@
+import csv
+import random
+
+cities = ['Buenos Aires', 'New York', 'San Francisco', 'London', 'Madrid', 'Paris', 'Berlin', 'Dubai', 'Lima', 'Sidney', 'Toronto', 'Tokyo', 'Beijing']
+corps = ['Google', 'Apple', 'Facebook', 'Amazon', 'Tesla', 'Tencent']
+events = 1000
+
+with open('Events.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, quotechar="'", quoting=csv.QUOTE_MINIMAL)
+    for event in range(events):
+        attendants = str(random.randint(10000,25000))
+        spamwriter.writerow([random.choice(cities)] + [random.choice(corps)] + [attendants])
