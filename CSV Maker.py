fb40ff976030386e9d842881c5e96636632b4aa1
nerthul11 <leandrorositto@yahoo.com.ar>
2020-10-16 00:13:39 -0300

    Agregado tutorial JS

--- CSV Maker.py
+++ CSV Maker.py
@@ -7,6 +7,7 @@ events = 1000
 
 with open('Events.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, quotechar="'", quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['City', 'Corps', 'Attendants'])
     for event in range(events):
         attendants = str(random.randint(10000,25000))
         spamwriter.writerow([random.choice(cities)] + [random.choice(corps)] + [attendants])
