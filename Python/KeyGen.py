2f43e8f2dfcd98b5c11c84838124af7fb1e09d37
nerthul11 <leandrorositto@yahoo.com.ar>
2020-10-20 16:14:24 -0300

    Added KeyGen.py

---
+++ Python/KeyGen.py
@@ -0,0 +1,12 @@
+import random
+
+# Non-route functions
+def code(length):
+    pw_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    pw_chars = list(pw_chars)
+    random.shuffle(pw_chars)
+    pw = random.choices(pw_chars, k=length)
+    pw = ''.join(pw)
+    return pw
+
+print(code(12))
